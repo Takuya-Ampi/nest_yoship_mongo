616063114d8652ba442ca57384698a71855145b6
行比拓也 <gyouhitakuya@MBP-2.local>
2020-10-21 13:31:45 +0900

    jwtライブラリ追加。loginメソッド実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, MongooseModule.forRoot('mongodb+srv://1234567890:1234567890@cluster0.tixz1.gcp.mongodb.net/nest?retryWrites=true&w=majority')],
+  imports: [UserModule, MongooseModule.forRoot('mongodb+srv://1234567890:1234567890@cluster0.tixz1.gcp.mongodb.net/nest?retryWrites=true&w=majority'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
